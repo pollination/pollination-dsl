3e5a7a0e7eec1c95177a6e525d18069915700e79
Mostapha <sadeghipour@gmail.com>
2020-12-26 00:13:36 -0500

    feat(function): expose additional information
    Added a number of useful properties to function including queenbee package information, python package information and outputs.

--- queenbee_dsl/function/__init__.py
+++ queenbee_dsl/function/__init__.py
@@ -1,5 +1,4 @@
 """All function related decorators and objects including inputs and outputs."""
-from dataclasses import dataclass
 from ._inputs import Inputs  # expose for easy import
 from ._outputs import Outputs  # expose for easy import
 from ._base import Function
