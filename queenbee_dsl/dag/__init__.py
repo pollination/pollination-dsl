2cda1cf0a79efb15b4d9eaf8fcdef07896ec434a
Mostapha <sadeghipour@gmail.com>
2021-01-01 21:03:53 -0500

    refactor: remove _ from module names
    This was the easiest way to ensure they will show up in docs.

--- queenbee_dsl/dag/__init__.py
+++ queenbee_dsl/dag/__init__.py
@@ -1,4 +1,4 @@
-from ._inputs import Inputs  # expose for easy import
-from ._outputs import Outputs
-from ._base import DAG
-from ._task import task
+from .inputs import Inputs  # expose for easy import
+from .outputs import Outputs
+from .base import DAG
+from .task import task
--- queenbee_dsl/function/__init__.py
+++ queenbee_dsl/function/__init__.py
@@ -1,4 +1,4 @@
 """All function related decorators and objects including inputs and outputs."""
-from ._inputs import Inputs  # expose for easy import
-from ._outputs import Outputs  # expose for easy import
-from ._base import Function, command
+from .inputs import Inputs  # expose for easy import
+from .outputs import Outputs  # expose for easy import
+from .base import Function, command
