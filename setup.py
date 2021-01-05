ef2dde26befd9e9a41710308982c274d213b9a8e
Mostapha Sadeghipour Roudsari <sadeghipour@gmail.com>
2021-01-04 20:31:51 -0500

    fix(setup.py): fix semantic versioning setup

--- setup.py
+++ setup.py
@@ -3,11 +3,19 @@ import setuptools
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open('README.md') as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="queenbee-dsl",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     author="Pollination",
     author_email="info@ladybug.tools",
     description="A Python DSL for Queenbee workflow language.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pollination/queenbee-python-dsl",
     packages=setuptools.find_packages(exclude=["tests"]),
     include_package_data=True,
     python_requires=">=3.7",
