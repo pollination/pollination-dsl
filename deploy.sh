bd89fd6b5147f3844647bc964ba2e080ce3a5d19
Mikkel Pedersen <mkklpdrsn@gmail.com>
2026-02-26 12:19:05 +0100

    fix(ci): Update dev deps and CI

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+echo "PyPi Deployment..."
 echo "Building distribution"
-python setup.py sdist bdist_wheel
+python -m build
 echo "Pushing new version to PyPi"
 twine upload dist/* -u $PYPI_USERNAME -p $PYPI_PASSWORD
